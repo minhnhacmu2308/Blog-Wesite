1093238cdd271a2446496cf9e7cf4448dba60bbc
minhnhacmu2308 <65648352+minhnhacmu2308@users.noreply.github.com>
2021-04-25 23:36:34 +0700

    new

---
+++ ssssss.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ssssss extends Component {
+    render() {
+        return (
+            <div>
+                <p>dd</p>
+            </div>
+        );
+    }
+}
+
+export default ssssss;
